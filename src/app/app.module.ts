f3d2e3db0f0a1524af67a9e0b6d21f9224519eaa
Marek Stančík <marekst11@gmail.com>
2021-09-20 11:47:48 +0200

    Fix angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MatSliderModule } from "@angular/material/slider";
 import { MatButtonModule } from "@angular/material/button";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { MatButtonModule } from "@angular/material/button";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MatSliderModule,
     MatButtonModule,
   ],
