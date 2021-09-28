429d1bb8a5f4c4ed0f9f8e2b0af85446bc658680
Marek Stančík <marekst11@gmail.com>
2021-09-28 10:22:07 +0200

    Added fancy stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BehaviorSubject, Observable, timer } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+  public smiley$: Observable<string> = timer(0,1000).pipe(map(num => num % 2 === 0 ? ":)" : ";)"));
 
+  // Humidity
+  public maxHumidity = 100;
+  
+  public humidity: number = 0;
+
+  private humidityStatus = new BehaviorSubject("Humidity is important");
+  
+  public get humidityStatus$(): Observable<any> {
+    return this.humidityStatus.pipe();
+  }
+  
+  // Temperature
+  public maxTemperature = 50;
+  
+  public minTemperature = 0;
+  
+  public temperature: number = 0;
+
+  private temperatureStatus = new BehaviorSubject("Right temperature makes me happy");
+
+  public get temperatureStatus$(): Observable<any> {
+    return this.temperatureStatus.pipe();
+  }
+
+  public onTempChange(ev: any) {
+    this.temperature = ev.value;
+    this.temperatureStatus.next(this.temperature > this.maxTemperature - this.maxTemperature / 3 ? "Thats too hot !" : this.temperature > this.maxTemperature / 3 ?  "I kinda like it :)" : "Could you warm me up ?");
+  }
+
+  public onHumidityChange(ev: any) {
+    this.humidity = ev.value;
+    this.humidityStatus.next(this.humidity > this.maxHumidity - this.maxHumidity / 3 ? "Too much water in the potter" : this.humidity > this.maxHumidity / 3 ? "I really enjoy this" : "Im too dry");
+  }
+
+  public submit() {
+    console.log("Test: ", this.humidity, this.temperature);
+  }
+
+  public displayHumidity(value: number) {
+    return value + "%";
+  }
+
+  public displayTemperature(value: number) {
+    return value + "°C";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { MatSliderModule } from "@angular/material/slider";
+import { FormsModule } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSliderModule } from "@angular/material/slider";
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from './app.component';
 
 
 @NgModule({
@@ -15,6 +17,8 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     BrowserAnimationsModule,
     MatSliderModule,
     MatButtonModule,
+    MatIconModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
